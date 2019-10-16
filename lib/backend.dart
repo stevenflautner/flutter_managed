c5cc82cb634ab560a97997207135667ed7faf673
StevenFlautner <steven.flautner@gmail.com>
2019-10-16 22:04:15 +0200

    repository

--- lib/backend.dart
+++ lib/backend.dart
@@ -5,20 +5,29 @@ import 'package:provider/provider.dart';
 void run({
   @required List<Object> repositories,
   @required List<Object> services,
+  @required List<SingleChildCloneableWidget> providers,
   @required ValueBuilder builder,
 }) {
   register(
     repositories: repositories,
     services: services
   );
-  runApp(App());
+  runApp(App(
+    providers: providers,
+    builder: builder,
+  ));
 }
 
 class App extends StatelessWidget {
-  final ValueBuilder builder;
 
-  const App({Key key, this.builder}) : super(key: key);
+  final ValueBuilder builder;
+  final List<SingleChildCloneableWidget> providers;
+
+  const App({Key key, @required this.providers, @required this.builder }) : super(key: key);
 
   @override
-  Widget build(BuildContext context) => builder(context);
+  Widget build(BuildContext context) => MultiProvider(
+    providers: providers,
+    child: builder(context),
+  );
 }
